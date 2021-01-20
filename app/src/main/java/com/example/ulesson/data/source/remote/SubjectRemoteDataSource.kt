c4bf1a60e2a1f951f7343dcb9b74de0737a829a2
deonolarewaju <deonolarewaju@gmail.com>
2021-01-20 21:44:17 +0100

    fix/video functions

--- app/src/main/java/com/example/ulesson/data/source/remote/SubjectRemoteDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ulesson.data.source.remote
-
-import com.example.ulesson.data.helper.Resource
-import com.example.ulesson.data.model.SubjectDataResponse
-
-interface SubjectRemoteDataSource {
-    suspend fun getSubjectData(): Resource<SubjectDataResponse>
-}
